a481acbc16ddd4a24345f59950a144370052f6ac
Ze Jin <zj58@en-cs-totient-01.coecis.cornell.edu>
2015-11-04 16:59:49 -0500

    update from pull request in cornell-cs5220-f15

--- fw-mpi.c
+++ fw-mpi.c
@@ -1,3 +1,4 @@
+#include <getopt.h>
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -56,7 +57,7 @@ static inline void deinfinitize(int n, int* l)
 /**
  *
  * Of course, any loop-free path in a graph with $n$ nodes can
- * at most pass theough every node in the graph.  Therefore,
+ * at most pass through every node in the graph.  Therefore,
  * once $2^s \geq n$, the quantity $l_{ij}^s$ is actually
  * the length of the shortest path of any number of hops.  This means
  * we can compute the shortest path lengths for all pairs of nodes
@@ -141,7 +142,7 @@ int* gen_graph(int n, double p)
  * arithmetic, we should get bitwise identical results from run to
  * run, even if we do optimizations that change the associativity of
  * our computations.  The function `fletcher16` computes a simple
- * [simple checksum][wiki-fletcher].  over the output of the
+ * [simple checksum][wiki-fletcher] over the output of the
  * `shortest_paths` routine, which we can then use to quickly tell
  * whether something has gone wrong.  The `write_matrix` routine
  * actually writes out a text representation of the matrix, in case we
