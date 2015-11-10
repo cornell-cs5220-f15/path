0042fbd665e379ca2199a5f0a8e21ce59a3f1a9e
Dylan Foster <turtlecannon@gmail.com>
2015-11-10 10:32:57 -0500

    Added timing code.

---
+++ size.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+touch indices.txt
+
+./submit 1000 | cut -d '.' -f 1 >> indices.txt
+./submit 2000 | cut -d '.' -f 1 >> indices.txt
+./submit 5000 | cut -d '.' -f 1 >> indices.txt
+./submit 10000 | cut -d '.' -f 1 >> indices.txt
+
+# ./submit wave 1000 40 | cut -d '.' -f 1 >> indices.txt
