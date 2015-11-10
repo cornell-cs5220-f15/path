17d8c0b60122ec340028e20e9b5fb2d7a5ab06de
Saul Toscano <toscano.saul@gmail.com>
2015-11-10 02:19:49 -0500

    mpi

--- path-mpi.c
+++ path-mpi.c
@@ -297,7 +297,7 @@ int main(int argc, char** argv)
     
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+    printf("%d",np);
     // Time the shortest paths code
     double t0 =MPI_Wtime();
     shortest_paths(n, l,np,rank);
