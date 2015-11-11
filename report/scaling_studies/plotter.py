Plotter fix to weak scaling

report/scaling_studies/plotter.py
@@ -23,9 +23,11 @@ plt.clf()
 plt.cla()
 
 threads, timings = np.loadtxt('weak_scaling.csv', delimiter=',', usecols=(0,1), unpack=True)
+serial_time = timings[0];
+timings = np.divide(serial_time, timings)
 
 plt.plot(threads, timings)
-plt.xlim([1,26])
+plt.xlim([1,20])
 plt.xlabel("Number of OMP threads")
 plt.ylabel("Efficiency")
 plt.savefig('weak_scaling.png', dpi=300)