3088cd026ef6ecef7be3e7c4c705bf6af5062baa
Dylan Foster <turtlecannon@gmail.com>
2015-11-18 22:47:30 -0500

    changes.

--- size.sh
+++ size.sh
@@ -4,7 +4,9 @@ touch indices.txt
 
 ./submit 1000 | cut -d '.' -f 1 >> indices.txt
 ./submit 2000 | cut -d '.' -f 1 >> indices.txt
-./submit 5000 | cut -d '.' -f 1 >> indices.txt
-./submit 10000 | cut -d '.' -f 1 >> indices.txt
+./submit 3000 | cut -d '.' -f 1 >> indices.txt
+./submit 4000 | cut -d '.' -f 1 >> indices.txt
+# ./submit 5000 | cut -d '.' -f 1 >> indices.txt
+# ./submit 10000 | cut -d '.' -f 1 >> indices.txt
 
 # ./submit wave 1000 40 | cut -d '.' -f 1 >> indices.txt
