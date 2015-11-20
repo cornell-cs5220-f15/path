7b6f3284b63332565e5622a47100c3794797bb86
sheroze1123 <mss385@cornell.edu>
2015-11-19 22:30:23 -0500

    Added scaling studies for MPI

--- report/scaling_studies/mpi_plotter.py
+++ report/scaling_studies/mpi_plotter.py
@@ -15,22 +15,28 @@ serial_time = timings[0];
 speedups = np.divide(serial_time, timings)
 
 plt.plot(procs, speedups)
-plt.xlim([1,26])
+x = np.linspace(1,21,100)
+y = [serial_time/0.511902] * len(x)
+plt.plot(x,y,'k--')
+plt.xlim([1,21])
 plt.xlabel("Number of processors")
 plt.ylabel("Speedup over a single processor")
-plt.title("Strong scaling study for MPI implementation")
+plt.title("Strong scaling study for MPI implementation with n=1000")
+legend = ['MPI scaling','Original solution baseline']
+plt.legend(legend, loc=7)
 plt.savefig('strong_scaling_mpi.png', dpi=200)
 plt.clf()
 plt.cla()
 
-threads, timings = np.loadtxt('weak_scaling.csv', delimiter=',', usecols=(0,1), unpack=True)
+threads, timings = np.loadtxt('weak_scaling_mpi.csv', delimiter=',', usecols=(0,1), unpack=True)
 serial_time = timings[0];
 timings = np.divide(serial_time, timings)
 
 plt.plot(threads, timings)
 plt.xlim([1,20])
-plt.xlabel("Number of OMP threads")
+plt.xlabel("Number of processors")
 plt.ylabel("Efficiency")
-plt.savefig('weak_scaling.png', dpi=300)
+plt.title("Weak scaling study for MPI implementation")
+plt.savefig('weak_scaling_mpi.png', dpi=200)
 plt.clf()
 plt.cla()
